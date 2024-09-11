0e97e951e7ba2ff4aaa687854dcf383cca867ff7
shyProgrammer351 <123141646+shyProgrammer351@users.noreply.github.com>
2024-09-11 16:49:58 +0900

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,6 @@
-// JAVA 프로그래밍 - https://codereading101.github.io/JAVA/
-// 소스파일 - https://github.com/CodeReading101/JAVA/blob/main/Chapter01/HelloWorld.java
-
-
-public class HelloWorld
+public class Main
 {
 	public static void main( String[] args ) {
-		// Hello, World!를 출력
 		System.out.println( "Hello, World! " );
 	}
 }
